c32a1e3907b10f9de3ccdc7f0e29bed39ac15f34
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-02 21:07:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,13 @@
 class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name
+    @this_dogs_name
+  end
+  def bark
+    puts "woof!"
+  end
 end
 
-fido = Dog.new
-fido.name = "Fido"
-
-puts fido.name
+  lassie = Dog.new
